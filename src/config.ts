4d38b7859f1e5607a7d7c5ae063ab258af047d15
Ryan Bouchou <106913590+iRyann@users.noreply.github.com>
2023-09-16 13:04:19 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Ryan Bouchou | Page personnelle';
+export const SITE_DESCRIPTION = 'Regroupemment de l\'lensemble des mes projets, cours et autres activités';
